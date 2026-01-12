0c8ac1148a6e304ed45a4af5e942562a12cdf423
pashq96 <pashq-90@mail.ru>
2026-01-12 17:37:55 +0100

    Логирование взаимодействий со страницей

--- components/base_component.py
+++ components/base_component.py
@@ -3,6 +3,9 @@ from typing import Pattern
 from playwright.sync_api import Page, expect
 
 import allure
+from tools.logger import get_logger
+
+logger = get_logger("BASE_COMPONENT")
 
 
 class BaseComponent:
@@ -10,7 +13,9 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+        with allure.step(step):
+            logger.info(step)
             expect(self.page).to_have_url(expected_url)
 
     def refresh_page(self):
